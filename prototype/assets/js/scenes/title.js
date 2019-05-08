ab3b978a2374a58f90ec592d31df6323254e568b
Dillon Yeh <dillony3h@gmail.com>
2019-05-08 14:46:17 -0700

    try integrating scenes

---
+++ prototype/assets/js/scenes/title.js
@@ -0,0 +1,35 @@
+import config from 'config.js'
+import Button from '../objects/button.js'
+
+export default class TitleScene extends Phaser.Scene {
+    constructor () {
+        super('Title');
+    }
+
+    create () {
+        // Game
+        this.gameButton = new Button(this, config.width/2, config.height/2 - 100, 'blueButton1', 'blueButton2', 'Play', 'Game');
+
+        // Options
+        this.optionsButton = new Button(this, config.width/2, config.height/2, 'blueButton1', 'blueButton2', 'Leaderboard', 'Leaderboard');
+
+        // Credits
+        this.creditsButton = new Button(this, config.width/2, config.height/2 + 100, 'blueButton1', 'blueButton2', 'Credits', 'Credits');
+
+    }
+
+
+    centerButton (gameObject, offset = 0) {
+        Phaser.Display.Align.In.Center(
+            gameObject,
+            this.add.zone(config.width/2, config.height/2 - offset * 100, config.width, config.height)
+        );
+    }
+
+    centerButtonText (gameText, gameButton) {
+        Phaser.Display.Align.In.Center(
+            gameText,
+            gameButton
+        );
+    }
+};
