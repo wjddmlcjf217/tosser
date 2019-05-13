d55aa9d937e179cd6a7013d1b8e7a9896a48d56e
Jacky Zheng <jackywzheng@gmail.com>
2019-05-13 15:47:26 -0700

    Added picture to credits and formatted

--- assets/js/scenes/credits.js
+++ assets/js/scenes/credits.js
@@ -5,43 +5,60 @@ export default class CreditsScene extends Phaser.Scene {
         super('Credits');
     }
 
+    preload () {
+        this.load.image('paper1', 'assets/img/paper_ball.png');
+        this.load.image('paper2', 'assets/img/paper_ball.png');
+        this.load.image('paper3', 'assets/img/paper_ball.png');
+        this.load.image('paper4', 'assets/img/paper_ball.png');
+        this.load.image('paper5', 'assets/img/paper_ball.png');
+    }
+
     create () {
         this.createBackground(this);
         this.creditsText = this.add.text(0, 0, 'Credits', LEADERBOARD_FONT);
         this.madeByText = this.add.text(0, 0, 'Created By: ', LEADERBOARD_FONT);
+        this.brydenPic = this.physics.add.image(window.innerWidth/2, 0, 'paper1');
         this.brydenText = this.add.text(0, 0, 'Bryden Mitchell', LEADERBOARD_FONT);
         this.brydenBio = this.add.text(0, 0, "'Fueled by Nat\'s Pizzeria'", LEADERBOARD_FONT);
+        this.dillonPic = this.physics.add.image(window.innerWidth/2, 0, 'paper2');
         this.dillonText = this.add.text(0, 0, 'Dillon Yeh', LEADERBOARD_FONT);
         this.dillonBio = this.add.text(0, 0, "'I have access to kids'", LEADERBOARD_FONT);
+        this.kevinPic = this.physics.add.image(window.innerWidth/2, 0, 'paper3');
         this.kevinText = this.add.text(0, 0, 'Kevin Jeong', LEADERBOARD_FONT);
         this.kevinBio = this.add.text(0, 0, "'DEEZ NUTS'", LEADERBOARD_FONT);
+        this.jackyPic = this.physics.add.image(window.innerWidth/2, 0, 'paper4');
         this.jackyText = this.add.text(0, 0, 'Jacky Zheng', LEADERBOARD_FONT);
         this.jackyBio = this.add.text(0, 0, "'All is fixed with var'", LEADERBOARD_FONT);
+        this.jaredPic = this.physics.add.image(window.innerWidth/2, 0, 'paper5');
         this.jaredText = this.add.text(0, 0, 'Jared Hall', LEADERBOARD_FONT);
         this.jaredBio = this.add.text(0, 0, "'We should add rimming'", LEADERBOARD_FONT);
         this.thankYou = this.add.text(0, 0, 'Thanks for playing!', LEADERBOARD_FONT);
         this.zone = this.add.zone(config.width/2, config.height/2, config.width, config.height);
-        this.tweenArray = [this.creditsText, this.madeByText, this.brydenText, this.brydenBio, this.dillonText,
-            this.dillonBio, this.kevinText, this.kevinBio, this.jackyText, this.jackyBio, this.jaredText, this.jaredBio,
-            this.thankYou];
-        const offset = -3500;
+        this.tweenArray = [this.creditsText, this.madeByText, this.brydenPic, this.brydenText, this.brydenBio,
+            this.dillonPic, this.dillonText, this.dillonBio, this.kevinPic, this.kevinText, this.kevinBio,
+            this.jackyPic, this.jackyText, this.jackyBio, this.jaredPic, this.jaredText, this.jaredBio, this.thankYou];
+        const offset = -5000;
 
         for (let i = 0; i < this.tweenArray.length; i++) {
             this.center(this.tweenArray[i])
         }
-
         this.madeByText.setY(1800);
-        this.brydenText.setY(2000);
-        this.brydenBio.setY(2100);
-        this.dillonText.setY(2300);
-        this.dillonBio.setY(2400);
-        this.kevinText.setY(2600);
-        this.kevinBio.setY(2700);
-        this.jackyText.setY(2900);
-        this.jackyBio.setY(3000);
-        this.jaredText.setY(3200);
-        this.jaredBio.setY(3300);
-        this.thankYou.setY(4300);
+        this.brydenPic.setY(2100);
+        this.brydenText.setY(2200);
+        this.brydenBio.setY(2300);
+        this.dillonPic.setY(2600);
+        this.dillonText.setY(2700);
+        this.dillonBio.setY(2800);
+        this.kevinPic.setY(3100);
+        this.kevinText.setY(3200);
+        this.kevinBio.setY(3300);
+        this.jackyPic.setY(3600);
+        this.jackyText.setY(3700);
+        this.jackyBio.setY(3800);
+        this.jaredPic.setY(4100);
+        this.jaredText.setY(4200);
+        this.jaredBio.setY(4300);
+        this.thankYou.setY(6000);
 
         this.creditsTween = this.tweens.add({
             targets: this.creditsText,
