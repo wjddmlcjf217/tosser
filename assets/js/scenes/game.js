e687aea9dc27afa59de4cf5282fee65fb9ffb959
brydenmitchell <brydengmitchell@gmaill.com>
2019-05-15 16:26:12 -0700

    - fix bug where you can throw hero after losing the games

--- assets/js/scenes/game.js
+++ assets/js/scenes/game.js
@@ -608,6 +608,7 @@ export default class GameScene extends Phaser.Scene {
         }
 
         if (scene.lives.countActive() < 1) {
+            scene.hero.disableInteractive();
             scene.staticScoreText.setVisible(false);
             scene.scoreText.setVisible(false);
             scene.gameOverText.setVisible(true);
