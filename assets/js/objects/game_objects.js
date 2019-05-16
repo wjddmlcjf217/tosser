01fd6b4fde36433b52c187a81dcf17e469dc8a42
Jacky Zheng <jackywzheng@gmail.com>
2019-05-16 12:41:01 -0700

    Added more facts to game_objects

--- assets/js/objects/game_objects.js
+++ assets/js/objects/game_objects.js
@@ -6,7 +6,6 @@ export default {
         'container': false,
         'organic': true,
         'paper': false,
-        'path': 'assets/img/banana-sprite.png',
         'scaling_factor': 1
     },
     'apple' : {
@@ -16,14 +15,16 @@ export default {
         'container': false,
         'organic': true,
         'paper': false,
-        'path': 'd',
         'scaling_factor': 0.605
     },
     // 'bread' : {
-    //     'description': 'd',
-    //     'bin': 'd',
-    //     'path': 'd',
-    //     'scaling_factor': 'd'
+    //     'fact_1': 'Sliced bread was invented in 1928!',
+    //     'fact_2': 'During WW2, fresh bread was illegal in the UK!',
+    //     'fact_3': 'Bread goes stale 6x faster in the fridge!',
+    //     'container': false,
+    //     'organic': true,
+    //     'paper': false,
+    //     'scaling_factor': 1
     // },
     'paper' : {
         'fact_1': 'It takes 5 litres of water to make one piece of paper!',
@@ -32,20 +33,25 @@ export default {
         'container': false,
         'organic': false,
         'paper': true,
-        'path': 'assets/img/paper_ball.png',
         'scaling_factor': 1
     },
     // 'cardboard' : {
-    //     'description': 'd',
-    //     'bin': 'd',
-    //     'path': 'd',
-    //     'scaling_factor': 'd'
+    //     'fact_1': 'Recycling cardboard takes 75% of the energy to make new cardboard!',
+    //     'fact_2': 'Recycling 1 ton of cardboard saves 46 gallons of oil!',
+    //     'fact_3': 'Cardboard is just thicker and more durable paper!',
+    //     'container': false,
+    //     'organic': false,
+    //     'paper': true,
+    //     'scaling_factor': 0.420
     // },
     // 'magazine' : {
-    //     'description': 'd',
-    //     'bin': 'd',
-    //     'path': 'd',
-    //     'scaling_factor': 'd'
+    //     'fact_1': 'Magazines were much more popular before the Internet!',
+    //     'fact_2': 'The glossy paper in magazines used to be unrecyclable, but not anymore!',
+    //     'fact_3': '50% of magazines are being recycled today!',
+    //     'container': false,
+    //     'organic': false,
+    //     'paper': true,
+    //     'scaling_factor': 0.420
     // },
     'waterbottle' : {
         'fact_1': 'Only 1 in 5 water bottles are recycled!',
@@ -58,15 +64,21 @@ export default {
         'scaling_factor': 0.420
     }
     // 'popcan' : {
-    //     'description': 'd',
-    //     'bin': 'd',
-    //     'path': 'd',
-    //     'scaling_factor': 'd'
+    //     'fact_1': 'Pop cans are made of aluminum, the most valuable recyclable material!',
+    //     'fact_2': '75% of aluminum produced in America is still in use today!',
+    //     'fact_3': 'Recycling aluminum saves 90% of energy required to make new aluminum!',
+    //     'container': true,
+    //     'organic': false,
+    //     'paper': false,
+    //     'scaling_factor': 0.420
     // },
-    // 'jug' : {
-    //     'description': 'd',
-    //     'bin': 'd',
-    //     'path': 'd',
-    //     'scaling_factor': 'd'
+    // 'carton' : {
+    //     'fact_1': 'Cartons are mainly made of paper and some plastic!',
+    //     'fact_2': 'In Canada, we are currently recycling 60% of cartons!',
+    //     'fact_3': 'Milk was originally stored in glass bottles until cartons came along!',
+    //     'container': true,
+    //     'organic': false,
+    //     'paper': false,
+    //     'scaling_factor': 0.420
     // }
 };
