c314b7473f314237c938d4edd1fe64a1f24aac4c
Kevin <kevin_ljeong@hotmail.com>
2019-05-21 12:15:56 -0700

    timer decrement issue solved in challengeMode.js

--- assets/js/scenes/challengeMode.js
+++ assets/js/scenes/challengeMode.js
@@ -1,12 +1,15 @@
 import GameScene from './game.js'
 
-export default class SubclassTest extends GameScene {
+export default class ChallengeMode extends GameScene {
     constructor() {
         super("ChallengeMode");
     }
 
     create() {
         super.create();
+
+        // this.optionButton.visible = false;
+
 
         // Create timer
         this.t = 30;
@@ -18,35 +21,25 @@ export default class SubclassTest extends GameScene {
 
         this.updateTimer(this);
 
-        //Option
-        this.optionButton = this.add.image(window.innerWidth * 0.945, window.innerHeight * 0.035, 'option');
-        this.optionButton.displayWidth = window.innerWidth * 0.10;
-        this.optionButton.displayHeight = window.innerHeight * 0.056;
-        this.optionButton.setInteractive();
-        this.optionButton.on('pointerdown', function() {
-            this.createOptions();
-            this.optionButton.visible = false;
-            this.hero.visible = false;
-            this.journalButton.visible = false;
-        }, this);
+        // this.mainMenu.on('pointerdown', function() {
+        //     clearInterval(this.timerInterval);
+        //     this.scene.start('Title');
+        // }, this);
+
     }
 
     updateTimer() {
-
-
-
-
-        console.log(this);
+        console.log(this.scene);
         this.timeText.setText('Timer: ' + this.t);
         this.t--;
         let thisScene = this;
-        let x = setInterval(function () {
+        thisScene.timerInterval = setInterval(function () {
             thisScene.timeText.setText('Timer: ' + thisScene.t);
-
+            // console.log(this);
             thisScene.t--;
             if (thisScene.t === -1) {
 
-                clearInterval(x);
+                clearInterval(thisScene.timerInterval);
                 thisScene.staticScoreText.setVisible(false);
                 thisScene.scoreText.setVisible(false);
                 thisScene.gameOverText.setVisible(true);
@@ -59,6 +52,35 @@ export default class SubclassTest extends GameScene {
 
             }
         }, 1000, thisScene);
+    }
+
+    createOptions() {
+        this.optionContainer = this.add.container(0,0);
+        let bg = this.add.image(window.innerWidth * 0.5, window.innerHeight * 0.5, 'options_background').setOrigin(0.5);
+        bg.displayWidth = window.innerWidth * 0.90;
+        bg.displayHeight = window.innerHeight * 0.60;
+        this.optionContainer.add(bg);
+        this.resumeGame = this.add.text(window.innerWidth * 0.5, window.innerHeight * 0.4, 'Resume', TITLE_FONT).setOrigin(0.5);
+        this.resumeGame.setFontSize(150);
+        this.resumeGame.setInteractive();
+        this.resumeGame.on('pointerdown', () => {
+            this.optionContainer.destroy();
+            this.journalButton.visible = true;
+            this.hero.visible = true;
+            this.optionButton.visible = true;
+        }, this);
+        this.optionContainer.add(this.resumeGame);
+        this.mainMenu = this.add.text(window.innerWidth * 0.5, window.innerHeight * 0.55, 'Main Menu', TITLE_FONT).setOrigin(0.5);
+        this.mainMenu.setFontSize(150);
+        this.mainMenu.setInteractive();
+        this.mainMenu.on('pointerdown', function() {
+            this.sound.stopAll('disco');
+            this.discoBall.removeInteractive();
+            clearInterval(this.timerInterval);
+            clearInterval(this.discoInterval);
+            this.scene.start('Title');
+        }, this);
+        this.optionContainer.add(this.mainMenu);
     }
 }
 
