305d8b0c51dd69923315ec1e3324f9fde0948db3
brydenmitchell <brydengmitchell@gmaill.com>
2019-05-22 13:28:07 -0700

    added comments to leaderboard.js

--- assets/js/scenes/leaderboard.js
+++ assets/js/scenes/leaderboard.js
@@ -1,10 +1,15 @@
-export default class TitleScene extends Phaser.Scene {
+/**
+ * LeaderBoard shows the user the top 8 scores from normal mode
+ */
+export default class LeaderBoard extends Phaser.Scene {
     constructor() {
         super('LeaderBoard');
     }
 
     create() {
+        // flag for when to draw leaderboard
         this.leaderboardCreated = false;
+        // create background
         this.createBackground();
 
         // Main Menu
@@ -17,7 +22,9 @@ export default class TitleScene extends Phaser.Scene {
     }
 
     update() {
+        // if the leaderboard has loaded
         if (this.leaderboardCreated === false) {
+            // prints the top 8 player scores to the leaderboard
             this.add.text(
                 window.innerWidth * 0.5, window.innerHeight * 0.1, "Leaderboard", LEADERBOARD_FONT
             ).setOrigin(0.5
@@ -37,6 +44,9 @@ export default class TitleScene extends Phaser.Scene {
         }
     }
 
+    /**
+     * Creates Background
+     */
     createBackground() {
         let background = this.add.image(window.innerWidth / 2, window.innerHeight / 2, 'background_blur');
         background.displayHeight = window.innerHeight;
