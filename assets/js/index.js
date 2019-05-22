bc405aca88872e5a01e7341056c6f86a556c06b7
brydenmitchell <brydengmitchell@gmaill.com>
2019-05-22 11:29:38 -0700

    added comments to index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,4 @@
+// import all phaser scenes
 import config from './scenes/config.js'
 import GameScene from './scenes/game.js'
 import TitleScene from './scenes/title.js'
@@ -9,10 +10,15 @@ import ChallengeMode from './scenes/challengeMode.js'
 import NavScene from './scenes/leaderboardNav.js'
 import ChallengeLeaderBoard from './scenes/challengeLeaderboard.js'
 
-
+/**
+ * Game class
+ *
+ * Handles game scene management
+ */
 class Game extends Phaser.Game {
     constructor () {
         super(config);
+        // loads game scenes
         this.scene.add('Loading', LoadingScene);
         this.scene.add('Title', TitleScene);
         this.scene.add('Nav', NavScene);
@@ -22,10 +28,12 @@ class Game extends Phaser.Game {
         this.scene.add('Credits', CreditsScene);
         this.scene.add('Tutorial', TutorialScene);
         this.scene.add('ChallengeMode', ChallengeMode);
+        // start loading scene
         this.scene.start('Loading');
     }
 }
 
+// pre-loads all fonts before launching the first scene
 WebFont.load({
     google: {
         families: ['Luckiest Guy', 'Kalam', 'Acme', 'Indie Flower']
@@ -33,6 +41,9 @@ WebFont.load({
     active: launchGame
 });
 
+/**
+ * Launches game instance
+ */
 function launchGame() {
     loadLeaderBoard().then(function () {
         window.game = new Game();
